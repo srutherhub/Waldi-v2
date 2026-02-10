a99b32f0088ddec403a7ab8fafb36eb3dc2ff852
srutherhub <samuelbrutherford@gmail.com>
2026-02-10 07:29:03 -0600

    add cache headers to a couple routes

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -3,30 +3,31 @@ package controllers
 import (
 	"fmt"
 	"net/http"
-	"waldi-v2/handlers"
-	"waldi-v2/services"
+	h "waldi-v2/handlers"
+	m "waldi-v2/handlers/middleware"
+	s "waldi-v2/services"
 )
 
 func Init(mux *http.ServeMux) {
-	appleMapsService := services.NewAppleMapsService()
-	appleMapsClient := services.NewAppleMapsClient(appleMapsService)
-	addressService := services.NewAddressService(appleMapsClient)
+	appleMapsService := s.NewAppleMapsService()
+	appleMapsClient := s.NewAppleMapsClient(appleMapsService)
+	addressService := s.NewAddressService(appleMapsClient)
 
 	baseRoute := New()
 	baseRoute.SetParentRoute("/")
-	baseRoute.RegisterRoute(Route{Method: "GET", Path: "", Handler: handlers.Homepage}, mux)
+	baseRoute.RegisterRoute(Route{Method: "GET", Path: "", Handler: m.SetCacheHeader(h.Homepage)}, mux)
 
 	resultRoute := New()
 	resultRoute.SetParentRoute("/result")
-	resultRoute.RegisterRoute(Route{Method: "GET", Path: "", Handler: handlers.Resultpage(addressService)}, mux)
-	resultRoute.RegisterRoute(Route{Method: "GET", Path: "/", Handler: handlers.Resultpage(addressService)}, mux)
-	resultRoute.RegisterRoute(Route{Method: "GET", Path: "/{id}", Handler: handlers.Resultpage(addressService)}, mux)
+	resultRoute.RegisterRoute(Route{Method: "GET", Path: "", Handler: h.Resultpage(addressService)}, mux)
+	resultRoute.RegisterRoute(Route{Method: "GET", Path: "/", Handler: h.Resultpage(addressService)}, mux)
+	resultRoute.RegisterRoute(Route{Method: "GET", Path: "/{id}", Handler: m.SetCacheHeader(h.Resultpage(addressService))}, mux)
 
 	apiFormRoute := New()
 	apiFormRoute.SetParentRoute("/api/form")
-	apiFormRoute.RegisterRoute(Route{Method: "POST", Path: "/address", Handler: handlers.AddressForm(addressService)}, mux)
-	apiFormRoute.RegisterRoute(Route{Method: "POST", Path: "/browserlocation", Handler: handlers.BrowserLocation(addressService)}, mux)
-		apiFormRoute.RegisterRoute(Route{Method: "POST", Path: "/mapmodal", Handler: handlers.MapModal()}, mux)
+	apiFormRoute.RegisterRoute(Route{Method: "POST", Path: "/address", Handler: h.AddressForm(addressService)}, mux)
+	apiFormRoute.RegisterRoute(Route{Method: "POST", Path: "/browserlocation", Handler: h.BrowserLocation(addressService)}, mux)
+	apiFormRoute.RegisterRoute(Route{Method: "POST", Path: "/mapmodal", Handler: m.SetCacheHeader(h.MapModal())}, mux)
 }
 
 type Route struct {
